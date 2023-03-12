cff709168cc0800702ffb0afdcf2bf0f3cde7350
F. G. Dorais <fgdorais@gmail.com>
2023-03-12 10:54:27 -0400

    fix: add Repr instance for Parser.Error.Simple

--- Parser/Error.lean
+++ Parser/Error.lean
@@ -15,6 +15,34 @@ inductive Simple (σ α) [Parser.Stream σ α]
 | unexpected : Stream.Position σ α → Option α → Simple σ α
 | addMessage : Simple σ α → Stream.Position σ α → String → Simple σ α
 
+-- The derive handler for `Repr` fails, this is a workaround.
+private def Simple.reprPrec {σ α} [Parser.Stream σ α] [Repr α] [Repr (Stream.Position σ α)] : Simple σ α → Nat → Std.Format
+| unexpected pos a, prec =>
+  Repr.addAppParen
+    (Std.Format.group
+      (Std.Format.nest (if prec >= max_prec then 1 else 2)
+        (Std.Format.text "Parser.Error.Simple.unexpected" ++
+          Std.Format.line ++
+          reprArg pos ++
+          Std.Format.line ++
+          reprArg a)))
+    prec
+| addMessage e pos msg, prec =>
+  Repr.addAppParen
+    (Std.Format.group
+      (Std.Format.nest (if prec >= max_prec then 1 else 2)
+        (Std.Format.text "Parser.Error.Simple.addMessage" ++
+          Std.Format.line ++
+          reprPrec e max_prec ++
+          Std.Format.line ++
+          reprArg pos ++
+          Std.Format.line ++
+        reprArg msg)))
+    prec
+
+instance (σ α) [Parser.Stream σ α] [Repr α] [Repr (Stream.Position σ α)] : Repr (Simple σ α) where
+  reprPrec := Simple.reprPrec
+
 private def Simple.toString {σ α} [Repr α] [Parser.Stream σ α] [Repr (Parser.Stream.Position σ α)] : Simple σ α → String
 | unexpected pos (some input) => s!"unexpected input {repr input} at {repr pos}"
 | unexpected pos none => s!"unexpected input at {repr pos}"
