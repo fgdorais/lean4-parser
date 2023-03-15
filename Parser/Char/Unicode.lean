d35e5ed5ccf59f01c12cff5ec972114db5b95a59
F. G. Dorais <fgdorais@gmail.com>
2023-03-15 13:07:58 -0400

    feat: more Unicode parsers

--- Parser/Char/Unicode.lean
+++ Parser/Char/Unicode.lean
@@ -8,206 +8,237 @@ import Parser.Char.Basic
 namespace Parser.Char.Unicode
 variable {ε σ m} [Parser.Stream σ Char] [Parser.Error ε σ Char] [Monad m] [MonadExceptOf ε m]
 
-/-- parse decimal digit character -/
-def decimalDigit : ParserT ε σ Char m (Fin 10) :=
-  withErrorMessage "expected decimal digit" do
-    tokenMap Unicode.toDigit?
+/-- parse alphabetic character -/
+def alphabetic : ParserT ε σ Char m Char :=
+  withErrorMessage "expected alphabetic character" do
+    tokenFilter Unicode.isAlphabetic
 
-/- General Category -/
+/-- parse lowercase letter character -/
+def lowercase : ParserT ε σ Char m Char :=
+  withErrorMessage "expected lowercase letter" do
+    tokenFilter Unicode.isLowercase
+
+/-- parse math symbol character -/
+def math : ParserT ε σ Char m Char :=
+  withErrorMessage "expected math symbol" do
+    tokenFilter Unicode.isMath
+
+/-- parse uppercase letter character -/
+def uppercase : ParserT ε σ Char m Char :=
+  withErrorMessage "expected uppercase letter" do
+    tokenFilter Unicode.isUppercase
+
+/-- parse white space character -/
+def whitespace : ParserT ε σ Char m Char :=
+  withErrorMessage "expected whitespace" do
+    tokenFilter Unicode.isWhiteSpace
+
+/-- parse decimal digit character -/
+def digit : ParserT ε σ Char m (Char × Fin 10) :=
+  withErrorMessage "expected decimal digit" do
+    tokenMap fun c => Unicode.toDigit? c >>= (c,.)
+
+/-!
+  ## General Category ##
+-/
 
 /-- parse character from given general category -/
 def parseGeneralCategory (category : Unicode.GeneralCategory) : ParserT ε σ Char m Char :=
   withErrorMessage s!"expected character of general category {category.toAbbrev}" do
     tokenFilter (Unicode.isInGeneralCategory . category)
 
+namespace GeneralCategory
+
 /-- parse letter (general category L) -/
 def letter : ParserT ε σ Char m Char :=
   withErrorMessage "expected letter (L)" do
-    tokenFilter Unicode.isLetter
+    tokenFilter Unicode.GeneralCategory.isLetter
 
 /-- parse cased letter (general category LC) -/
 def casedLetter : ParserT ε σ Char m Char :=
   withErrorMessage "expected cased letter (LC)" do
-    tokenFilter Unicode.isCasedLetter
+    tokenFilter Unicode.GeneralCategory.isCasedLetter
 
 /-- parse lowercase letter (general category Ll) -/
 def lowercaseLetter : ParserT ε σ Char m Char :=
   withErrorMessage "expected lowercase letter (Ll)" do
-    tokenFilter Unicode.isLowercaseLetter
+    tokenFilter Unicode.GeneralCategory.isLowercaseLetter
 
 /-- parse uppercase letter (general category Lu) -/
 def uppercaseLetter : ParserT ε σ Char m Char :=
   withErrorMessage "expected uppercase letter (Lu)" do
-    tokenFilter Unicode.isUppercaseLetter
+    tokenFilter Unicode.GeneralCategory.isUppercaseLetter
 
 /-- parse titlecase letter (general category Lt) -/
 def titlecaseLetter : ParserT ε σ Char m Char :=
   withErrorMessage "expected titlecase letter (Lt)" do
-    tokenFilter Unicode.isTitlecaseLetter
+    tokenFilter Unicode.GeneralCategory.isTitlecaseLetter
 
 /-- parse other letter (general category Lm) -/
 def modifierLetter : ParserT ε σ Char m Char :=
   withErrorMessage "expected modifier letter (Lm)" do
-    tokenFilter Unicode.isModifierLetter
+    tokenFilter Unicode.GeneralCategory.isModifierLetter
 
 /-- parse other letter (general category Lo) -/
 def otherLetter : ParserT ε σ Char m Char :=
   withErrorMessage "expected other letter (Lo)" do
-    tokenFilter Unicode.isOtherLetter
+    tokenFilter Unicode.GeneralCategory.isOtherLetter
 
 /-- parse mark (general category M) -/
 def mark : ParserT ε σ Char m Char :=
   withErrorMessage "expected mark (M)" do
-    tokenFilter Unicode.isMark
+    tokenFilter Unicode.GeneralCategory.isMark
 
 /-- parse spacing combining mark (general category Mc) -/
 def spacingMark : ParserT ε σ Char m Char :=
   withErrorMessage "expected spacing mark (Mc)" do
-    tokenFilter Unicode.isSpacingMark
+    tokenFilter Unicode.GeneralCategory.isSpacingMark
 
 /-- parse nonspacing combining mark (general category Mn) -/
 def nonspacingMark : ParserT ε σ Char m Char :=
   withErrorMessage "expected nonspacing mark (Mn)" do
-    tokenFilter Unicode.isNonspacingMark
+    tokenFilter Unicode.GeneralCategory.isNonspacingMark
 
 /-- parse enclosing combining mark (general category Me) -/
 def enclosingMark : ParserT ε σ Char m Char :=
   withErrorMessage "expected enclosing mark (Me)" do
-    tokenFilter Unicode.isEnclosingMark
+    tokenFilter Unicode.GeneralCategory.isEnclosingMark
 
 /-- parse number (general category N) -/
 def number : ParserT ε σ Char m Char :=
   withErrorMessage "expected number (N)" do
-    tokenFilter Unicode.isNumber
+    tokenFilter Unicode.GeneralCategory.isNumber
 
 /-- parse decimal number (general category Nd) -/
 def decimalNumber : ParserT ε σ Char m Char :=
   withErrorMessage "expected decimal number (Nd)" do
-    tokenFilter Unicode.isDecimalNumber
+    tokenFilter Unicode.GeneralCategory.isDecimalNumber
 
 /-- parse letter number (general category Nl) -/
 def letterNumber : ParserT ε σ Char m Char :=
   withErrorMessage "expected letter number (Nl)" do
-    tokenFilter Unicode.isLetterNumber
+    tokenFilter Unicode.GeneralCategory.isLetterNumber
 
 /-- parse other number (general category No) -/
 def otherNumber : ParserT ε σ Char m Char :=
   withErrorMessage "expected other number (No)" do
-    tokenFilter Unicode.isOtherNumber
+    tokenFilter Unicode.GeneralCategory.isOtherNumber
 
 /-- parse punctuation (general category P) -/
 def punctuation : ParserT ε σ Char m Char :=
   withErrorMessage "expected punctuation (P)" do
-    tokenFilter Unicode.isPunctuation
+    tokenFilter Unicode.GeneralCategory.isPunctuation
 
 /-- parse connector punctuation (general category Pc) -/
 def connectorPunctuation : ParserT ε σ Char m Char :=
   withErrorMessage "expected connector punctuation (Pc)" do
-    tokenFilter Unicode.isConnectorPunctuation
+    tokenFilter Unicode.GeneralCategory.isConnectorPunctuation
 
 /-- parse dash punctuation (general category Pd) -/
 def dashPunctuation : ParserT ε σ Char m Char :=
   withErrorMessage "expected dash punctuation (Pd)" do
-    tokenFilter Unicode.isDashPunctuation
+    tokenFilter Unicode.GeneralCategory.isDashPunctuation
 
 /-- parse opening punctuation (general category Ps) -/
 def openPunctuation : ParserT ε σ Char m Char :=
   withErrorMessage "expected opening punctuation (Ps)" do
-    tokenFilter Unicode.isOpenPunctuation
+    tokenFilter Unicode.GeneralCategory.isOpenPunctuation
 
 /-- parse closing punctuation (general category Pe) -/
 def closePunctuation : ParserT ε σ Char m Char :=
   withErrorMessage "expected opening punctuation (Pe)" do
-    tokenFilter Unicode.isClosePunctuation
+    tokenFilter Unicode.GeneralCategory.isClosePunctuation
 
 /-- parse initial punctuation (general category Pi) -/
 def initialPunctuation : ParserT ε σ Char m Char :=
   withErrorMessage "expected initial punctuation (Pi)" do
-    tokenFilter Unicode.isInitialPunctuation
+    tokenFilter Unicode.GeneralCategory.isInitialPunctuation
 
 /-- parse final punctuation (general category Pf) -/
 def finalPunctuation : ParserT ε σ Char m Char :=
   withErrorMessage "expected final punctuation (Pf)" do
-    tokenFilter Unicode.isFinalPunctuation
+    tokenFilter Unicode.GeneralCategory.isFinalPunctuation
 
 /-- parse other punctuation (general category Po) -/
 def otherPunctuation : ParserT ε σ Char m Char :=
   withErrorMessage "expected other punctuation (Po)" do
-    tokenFilter Unicode.isOtherPunctuation
+    tokenFilter Unicode.GeneralCategory.isOtherPunctuation
 
 /-- parse symbol (general category S) -/
 def symbol : ParserT ε σ Char m Char :=
   withErrorMessage "expected symbol (S)" do
-    tokenFilter Unicode.isSymbol
+    tokenFilter Unicode.GeneralCategory.isSymbol
 
 /-- parse math symbol (general category Sm) -/
 def mathSymbol : ParserT ε σ Char m Char :=
   withErrorMessage "expected math symbol (Sm)" do
-    tokenFilter Unicode.isMathSymbol
+    tokenFilter Unicode.GeneralCategory.isMathSymbol
 
 /-- parse currency symbol (general category Sc) -/
 def currencySymbol : ParserT ε σ Char m Char :=
   withErrorMessage "expected currency symbol (Sc)" do
-    tokenFilter Unicode.isCurrencySymbol
+    tokenFilter Unicode.GeneralCategory.isCurrencySymbol
 
 /-- parse modifier symbol (general category Sk) -/
 def modifierSymbol : ParserT ε σ Char m Char :=
   withErrorMessage "expected modifier symbol (Sk)" do
-    tokenFilter Unicode.isModifierSymbol
+    tokenFilter Unicode.GeneralCategory.isModifierSymbol
 
 /-- parse other symbol (general category So) -/
 def otherSymbol : ParserT ε σ Char m Char :=
   withErrorMessage "expected other symbol (So)" do
-    tokenFilter Unicode.isOtherSymbol
+    tokenFilter Unicode.GeneralCategory.isOtherSymbol
 
 /-- parse separator (general category Z) -/
 def separator : ParserT ε σ Char m Char :=
   withErrorMessage "expected separator (Z)" do
-    tokenFilter Unicode.isSeparator
+    tokenFilter Unicode.GeneralCategory.isSeparator
 
 /-- parse space separator (general category Zs) -/
 def spaceSeparator : ParserT ε σ Char m Char :=
   withErrorMessage "expected space separator (Zs)" do
-    tokenFilter Unicode.isSpaceSeparator
+    tokenFilter Unicode.GeneralCategory.isSpaceSeparator
 
 /-- parse line separator (general category Zl) -/
 def lineSeparator : ParserT ε σ Char m Char :=
   withErrorMessage "expected line separator (Zl)" do
-    tokenFilter Unicode.isLineSeparator
+    tokenFilter Unicode.GeneralCategory.isLineSeparator
 
 /-- parse paragraph separator (general category Zp) -/
 def paragraphSeparator : ParserT ε σ Char m Char :=
   withErrorMessage "expected paragraph separator (Zp)" do
-    tokenFilter Unicode.isParagraphSeparator
+    tokenFilter Unicode.GeneralCategory.isParagraphSeparator
 
 /-- parse other character (general category C) -/
 def other : ParserT ε σ Char m Char :=
   withErrorMessage "expected other character (C)" do
-    tokenFilter Unicode.isOther
+    tokenFilter Unicode.GeneralCategory.isOther
 
 /-- parse control character (general category Cc) -/
 def control : ParserT ε σ Char m Char :=
   withErrorMessage "expected control character (Cc)" do
-    tokenFilter Unicode.isControl
+    tokenFilter Unicode.GeneralCategory.isControl
 
 /-- parse format character (general category Cf) -/
 def format : ParserT ε σ Char m Char :=
   withErrorMessage "expected format character (Cf)" do
-    tokenFilter Unicode.isFormat
+    tokenFilter Unicode.GeneralCategory.isFormat
 
 /-- parse surrogate character (general category Cs) -/
 def surrogate : ParserT ε σ Char m Char :=
   withErrorMessage "expected surrogate character (Cs)" do
-    tokenFilter Unicode.isSurrogate
+    tokenFilter Unicode.GeneralCategory.isSurrogate
 
 /-- parse private-use character (general category Co) -/
 def privateUse : ParserT ε σ Char m Char :=
   withErrorMessage "expected private-use character (Co)" do
-    tokenFilter Unicode.isPrivateUse
+    tokenFilter Unicode.GeneralCategory.isPrivateUse
 
 /-- parse unassigned character (general category Cn) -/
 def noncharacter : ParserT ε σ Char m Char :=
   withErrorMessage "expected unassigned character (Cn)" do
-    tokenFilter Unicode.isUnassigned
+    tokenFilter Unicode.GeneralCategory.isUnassigned
+
+end GeneralCategory
 
 end Parser.Char.Unicode
