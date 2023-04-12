b6b625dcdd63c7e726d7f7ebcd6317e3bc8a635f
F. G. Dorais <fgdorais@gmail.com>
2023-04-12 09:58:48 -0400

    fix: missing `outParam` at `Parser.Stream`, `Parser.Error` and add reducible instances

--- Parser/Error.lean
+++ Parser/Error.lean
@@ -7,21 +7,21 @@ import Parser.Prelude
 import Parser.Stream
 
 /-- parser error type -/
-protected class Parser.Error (ε σ α : Type _) [Parser.Stream σ α] where
+protected class Parser.Error (ε σ : Type _) (α : outParam (Type _)) [Parser.Stream σ α] where
   /-- unexpected input -/
-  unexpected : Stream.Position σ α → Option α → ε
+  unexpected : Stream.Position σ → Option α → ε
   /-- add error message -/
-  addMessage : ε → Stream.Position σ α → String → ε
+  addMessage : ε → Stream.Position σ → String → ε
 
 namespace Parser.Error
 
 /-- simple error type -/
 inductive Simple (σ α) [Parser.Stream σ α]
-| unexpected : Stream.Position σ α → Option α → Simple σ α
-| addMessage : Simple σ α → Stream.Position σ α → String → Simple σ α
+| unexpected : Stream.Position σ → Option α → Simple σ α
+| addMessage : Simple σ α → Stream.Position σ → String → Simple σ α
 
 -- The derive handler for `Repr` fails, this is a workaround.
-private def Simple.reprPrec {σ α} [Parser.Stream σ α] [Repr α] [Repr (Stream.Position σ α)] : Simple σ α → Nat → Std.Format
+private def Simple.reprPrec {σ α} [Parser.Stream σ α] [Repr α] [Repr (Stream.Position σ)] : Simple σ α → Nat → Std.Format
 | unexpected pos a, prec =>
   Repr.addAppParen
     (Std.Format.group
@@ -45,15 +45,15 @@ private def Simple.reprPrec {σ α} [Parser.Stream σ α] [Repr α] [Repr (Strea
         reprArg msg)))
     prec
 
-instance (σ α) [Parser.Stream σ α] [Repr α] [Repr (Stream.Position σ α)] : Repr (Simple σ α) where
+instance (σ α) [Parser.Stream σ α] [Repr α] [Repr (Stream.Position σ)] : Repr (Simple σ α) where
   reprPrec := Simple.reprPrec
 
-private def Simple.toString {σ α} [Repr α] [Parser.Stream σ α] [Repr (Parser.Stream.Position σ α)] : Simple σ α → String
+private def Simple.toString {σ α} [Repr α] [Parser.Stream σ α] [Repr (Parser.Stream.Position σ)] : Simple σ α → String
 | unexpected pos (some input) => s!"unexpected input {repr input} at {repr pos}"
 | unexpected pos none => s!"unexpected input at {repr pos}"
 | addMessage e pos msg => Simple.toString e ++ s!"; {msg} at {repr pos}"
 
-instance (σ α) [Repr α] [Parser.Stream σ α] [Repr (Parser.Stream.Position σ α)] : ToString (Simple σ α) where
+instance (σ α) [Repr α] [Parser.Stream σ α] [Repr (Parser.Stream.Position σ)] : ToString (Simple σ α) where
   toString e := Simple.toString e
 
 instance  (σ α) [Parser.Stream σ α] : Parser.Error (Simple σ α) σ α where
--- Parser/Stream.lean
+++ Parser/Stream.lean
@@ -6,27 +6,30 @@ Released under Apache 2.0 license as described in the file LICENSE.
 import Parser.Prelude
 
 /-- `Parser.Stream` class extends `Stream` with position features -/
-protected class Parser.Stream (σ α : Type _) extends Stream σ α where
-  /-- `Parser.Stream` position type -/
+protected class Parser.Stream (σ : Type _) (α : outParam (Type _)) extends Stream σ α where
+  /-- Position type -/
   Position : Type _
-  /-- get current `Parser.Stream` position -/
+  /-- Get current stream position -/
   getPosition : σ → Position
-  /-- set current `Parser.Stream` position -/
-  setPosition (s : σ) : Position → σ
+  /-- Set current stream position -/
+  setPosition : σ → Position → σ
 attribute [reducible] Parser.Stream.Position
 
 namespace Parser.Stream
 
-/-- wrapper to make a `Parser.Stream` from a plain `Stream` -/
-@[nolint unusedArguments] def mkDefault (σ α) [Stream σ α] := σ
+/-- Wrapper to make a `Parser.Stream` from a core `Stream` -/
+@[nolint unusedArguments]
+def mkDefault (σ α) [Stream σ α] := σ
 
-@[reducible] instance (σ α) [self : Stream σ α] : Parser.Stream (mkDefault σ α) α where
+@[reducible]
+instance (σ α) [self : Stream σ α] : Parser.Stream (mkDefault σ α) α where
   toStream := self
   Position := σ
   getPosition s := s
   setPosition _ p := p
 
-@[reducible] instance : Parser.Stream Substring Char where
+@[reducible]
+instance : Parser.Stream Substring Char where
   Position := String.Pos
   getPosition s := s.startPos
   setPosition s p :=
@@ -34,7 +37,8 @@ namespace Parser.Stream
     then {s with startPos := p}
     else {s with startPos := s.stopPos}
 
-@[reducible] instance (α) : Parser.Stream (Subarray α) α where
+@[reducible]
+instance (α) : Parser.Stream (Subarray α) α where
   Position := Nat
   getPosition s := s.start
   setPosition s p :=
@@ -42,7 +46,8 @@ namespace Parser.Stream
     then {s with start := p, h₁ := h}
     else {s with start := s.stop, h₁ := Nat.le_refl s.stop}
 
-@[reducible] instance : Parser.Stream ByteSubarray UInt8 where
+@[reducible]
+instance : Parser.Stream ByteSubarray UInt8 where
   Position := Nat
   getPosition s := s.start
   setPosition s p :=
@@ -51,14 +56,14 @@ namespace Parser.Stream
     else
       {s with start := s.stop, size := 0, valid := by rw [ByteSubarray.stop]; exact s.valid}
 
-/-- `OfList` is a view of an `List` along with a position along that array -/
+/-- `OfList` is a view of a list along with a position along that list -/
 structure OfList (α : Type _) where
-  /-- remaining tokens -/
+  /-- Remaining tokens -/
   next : List α
-  /-- consumed tokens -/
+  /-- Consumed tokens -/
   past : List α := []
 
-/-- set position -/
+/-- Set position -/
 def OfList.setPosition {α} (s : OfList α) (p : Nat) : OfList α :=
   let rec fwd : Nat → OfList α → OfList α
   | k+1, ⟨x :: rest, past⟩ => fwd k ⟨rest, x :: past⟩
@@ -71,11 +76,12 @@ def OfList.setPosition {α} (s : OfList α) (p : Nat) : OfList α :=
   else
     rev (s.past.length - p) s
 
-/-- make a `Parser.Stream` from a `List` -/
-@[inline] def mkOfList {α} (data : List α) (pos : Nat := 0) : OfList α :=
+/-- Make a `Parser.Stream` from a `List` -/
+def mkOfList {α} (data : List α) (pos : Nat := 0) : OfList α :=
   OfList.setPosition {next := data} pos
 
-@[reducible] instance (α) : Parser.Stream (OfList α) α where
+@[reducible]
+instance (α) : Parser.Stream (OfList α) α where
   Position := Nat
   getPosition s := s.past.length
   setPosition := OfList.setPosition
