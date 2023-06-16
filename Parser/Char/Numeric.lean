fd142c5e3292d38beb26e5ca499f52521a5453f2
F. G. Dorais <fgdorais@gmail.com>
2023-06-16 08:47:24 -0400

    fix: fix missing monad

--- Parser/Char/Numeric.lean
+++ Parser/Char/Numeric.lean
@@ -10,31 +10,31 @@ namespace Parser.Char.ASCII
 variable {ε σ m} [Parser.Stream σ Char] [Parser.Error ε σ Char] [Monad m] [MonadExceptOf ε m]
 
 @[inline]
-private def decNum (n : Nat := 0) : Parser ε σ Char (Nat × Nat) :=
+private def decNum (n : Nat := 0) : ParserT ε σ Char m (Nat × Nat) :=
   foldl (fun (r : Nat × Nat) (d : Fin 10) => (10 * r.1 + d, r.2+1)) (pure (n,0)) ASCII.digit
 
 @[inline]
-private def binNum (n : Nat := 0) : Parser ε σ Char (Nat × Nat) :=
+private def binNum (n : Nat := 0) : ParserT ε σ Char m (Nat × Nat) :=
   foldl (fun (r : Nat × Nat) (d : Fin 2) => (r.1 <<< 1 + d, r.2+1)) (pure (n,0)) ASCII.binDigit
 
 @[inline]
-private def octNum (n : Nat := 0) : Parser ε σ Char (Nat × Nat) :=
+private def octNum (n : Nat := 0) : ParserT ε σ Char m (Nat × Nat) :=
   foldl (fun (r : Nat × Nat) (d : Fin 8) => (r.1 <<< 3 + d, r.2+1)) (pure (n,0)) ASCII.octDigit
 
 @[inline]
-private def hexNum (n : Nat := 0) : Parser ε σ Char (Nat × Nat) :=
+private def hexNum (n : Nat := 0) : ParserT ε σ Char m (Nat × Nat) :=
   foldl (fun (r : Nat × Nat) (d : Fin 16) => (r.1 <<< 4 + d, r.2+1)) (pure (n,0)) ASCII.hexDigit
 
 /-- Parse a `Nat` -/
-def parseNat (decimalOnly := true) : Parser ε σ Char Nat := do
+def parseNat (decimalOnly := true) : ParserT ε σ Char m Nat := do
   match ← ASCII.digit with
   | ⟨0, _⟩ =>
     if decimalOnly then
       Prod.fst <$> ASCII.decNum
     else
       first [
-        (char 'b' <|> char 'B') *> binNum,
-        (char 'x' <|> char 'X') *> hexNum,
+        char 'b' *> binNum,
+        char 'x' *> hexNum,
         octNum,
         return 0]
   | ⟨n, _⟩ => Prod.fst <$> ASCII.decNum n
@@ -50,14 +50,14 @@ where
     Prod.fst <$> ASCII.hexNum n
 
 /-- Parse an `Int` -/
-def parseInt (decimalOnly := true) : Parser ε σ Char Int := do
+def parseInt (decimalOnly := true) : ParserT ε σ Char m Int := do
   match ← option? (char '+' <|> char '-') with
   | some '-' => Int.negOfNat <$> parseNat decimalOnly
   | _ => Int.ofNat <$> parseNat decimalOnly
 
 /-- Parse scientific notation -/
 @[inline]
-def parseScientific (α) [OfScientific α] : Parser ε σ Char α := do
+def parseScientific (α) [OfScientific α] : ParserT ε σ Char m α := do
   let (man, pre) ← ASCII.decNum
   let (man, aft) ← (char '.' *> ASCII.decNum man) <|> pure (man, 0)
   if pre + aft = 0 then throwUnexpected
@@ -65,7 +65,7 @@ def parseScientific (α) [OfScientific α] : Parser ε σ Char α := do
   return OfScientific.ofScientific man (exp < aft) (exp - aft).natAbs
 
 /-- Parse a `Float` -/
-def parseFloat : Parser ε σ Char Float := do
+def parseFloat : ParserT ε σ Char m Float := do
   match ← option? (char '+' <|> char '-') with
   | some '-' => Float.neg <$> parseScientific Float
   | _ => parseScientific Float
