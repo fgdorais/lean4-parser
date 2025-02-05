7af8783b58c250c4e0cd8381da4e7a04b2cb29be
F. G. Dorais <fgdorais@gmail.com>
2025-02-05 13:05:04 -0500

    doc: annotate JSON example

--- examples/JSON.lean
+++ examples/JSON.lean
@@ -21,92 +21,255 @@ open Parser Char
 /-- JSON parser monad -/
 protected abbrev Parser := SimpleParser Substring Char
 
-/-- Parse JSON white spaces -/
+/-- Parse JSON white spaces
+
+```
+ws
+  | ""
+  | ' ' ws
+  | '\n' ws
+  | '\r' ws
+  | '\t' ws
+```
+-/
 def ws : JSON.Parser Unit :=
   dropMany <| tokenFilter fun c => c == ' ' || c == '\n' || c == '\r' || c == '\t'
 
-/-- Parse a JSON number -/
+/-- Parse a JSON number
+
+Specification:
+```
+number
+  | integer fraction exponent
+```
+-/
 protected partial def number : JSON.Parser Unit :=
   withErrorMessage "expected number" do
-    let _ ← optional (char '-')
-    match ← ASCII.digit with
-    | ⟨0, _⟩ => pure ()
-    | _ => dropMany ASCII.digit
-    optional do
-      let _ ← char '.'
-      dropMany1 ASCII.digit
-    optional do
-      let _ ← char 'E' <|> char 'e'
-      let _ ← optional (char '+' <|> char '-')
-      dropMany1 ASCII.digit
 
-/-- Parse a JSON string -/
+    /- Integer part
+
+    Specification:
+    ```
+    integer
+      | digit
+      | onenine digits
+      | '-' digit
+      | '-' onenine digits
+
+    onenine
+      | '1' . '9'
+
+    digit
+      | '0'
+      | onenine
+
+    digits
+      | digit
+      | digit digits
+    ```
+
+    The following implementation uses an equivalent specification.
+    A JSON integer consists of an optional negative sign ('-') followed by either the single
+    digit '0' or a nonzero digjt ('1' . '9') followed by zero or more digits.
+    In other words, we follow this alternative specification:
+    ```
+    integer
+      | '0'
+      | onenine decimals
+
+    decimals
+      | ""
+      | decimal decimals
+
+    decimal
+      | '0' . '9'
+
+    onenine
+      | '1' . '9'
+    ```
+    -/
+    let _ ← optional (char '-')                 -- "" | '-'
+    match ← ASCII.digit with
+    | ⟨0, _⟩ => pure ()                         -- '0'
+    | _ => dropMany ASCII.digit                 -- onenine | onenine decimals
+
+    /- Fraction part
+
+    Specification:
+    ```
+    fraction
+      | ""
+      | '.' digits
+    ```
+    -/
+    optional do                                 -- "" | ...
+      let _ ← char '.'                          -- '.'
+      dropMany1 ASCII.digit                     -- digits
+
+    /- Exponent part
+
+    Specification:
+    ```
+    exponent
+      | ""
+      | e sign digits
+      | E sign digits
+
+    sign
+      | ""
+      | '+'
+      | '-'
+    ```
+    -/
+    optional do                                 -- "" | ...
+      let _ ← char 'e' <|> char 'E'             -- 'e' | 'E'
+      let _ ← optional (char '+' <|> char '-')  -- "" | '+' | '-'
+      dropMany1 ASCII.digit                     -- digits
+
+/-- Parse a JSON string
+
+Specification:
+```
+string
+  | '"' characters '"'
+
+characters
+  | ""
+  | character characters
+
+character
+  | U+0020 . U+10FFFF - '"' - '\'
+  | '\' escape
+```
+Note that the code points before U+0020 (space) are control characters.
+-/
 protected def string : JSON.Parser Unit :=
   withErrorMessage "expected string" do
-    let _ ← char '"'
-    let _ ← dropUntil (char '"') do
+    let _ ← char '"'                            -- '"'
+    let _ ← dropUntil (char '"') do             -- ... | '"'
       match ← anyToken with
-      | '\\' => escape
-      | c =>
+      | '\\' => escape                          -- '\' escape
+      | c =>                                    -- any other character
         if c.val ≥ 0x20 then
           pure ()
-        else
+        else                                    -- except control characters
           withErrorMessage "unexpected control character" throwUnexpected
 where
-  /-- Parse an escape character -/
+
+  /-- Escaped character
+
+  Specification:
+  ```
+  escape
+    | '"'
+    | '\'
+    | '/'
+    | 'b'
+    | 'f'
+    | 'n'
+    | 'r'
+    | 't'
+    | 'u' hex hex hex hex
+
+  hex
+    | '0' . '9'
+    | 'A' . 'F'
+    | 'a' . 'f'
+  ```
+  -/
   escape : JSON.Parser Unit :=
     withErrorMessage "expected escape" do
       match ← anyToken with
-      | '"' | '\\' | '/' | 'b' | 'f' | 'n' | 'r' | 't' => pure ()
-      | 'u' => drop 4 ASCII.hexDigit
+      | '"' | '\\' | '/' | 'b'
+      | 'f' | 'n' | 'r' | 't' => pure ()        -- '"' | '\' | '/' | 'b' | 'f' | 'n' | 'r' | 't'
+      | 'u' => drop 4 ASCII.hexDigit            -- 'u' hex hex hex hex
       | c => throwUnexpected c
 
 mutual
 
-/-- Parse a JSON object -/
+/-- Parse a JSON value
+
+Specification:
+```
+value
+  | object
+  | array
+  | string
+  | number
+  | "true"
+  | "false"
+  | "null"
+```
+The `object` and `array` parsers recursively
+depend on `value` so they are in a mutual
+declaration block.
+-/
+protected partial def value : JSON.Parser Unit :=
+  first [
+    JSON.object,
+    JSON.array,
+    JSON.string,
+    JSON.number,
+    string "true" *> pure (),
+    string "false" *> pure (),
+    string "null" *> pure (),
+    throwUnexpectedWithMessage none "expected value"
+  ]
+
+/-- Parse a JSON object
+
+Specification:
+```
+object
+  | '{' ws '}'
+  | '{' members '}'
+
+members
+  | member
+  | member ',' members
+
+member
+  | ws string ws ':' ws value ws
+```
+-/
 protected partial def object : JSON.Parser Unit :=
   withErrorMessage "expected object" do
-    let _ ← char '{'
-    let _ ← sepBy (char ',') do
-      let _ ← ws *> JSON.string
-      let _ ← ws *> char ':'
-      let _ ← ws *> JSON.value
-    let _ ← ws <* char '}'
+    let _ ← char '{'                            -- '{'
+    let _ ← sepBy (ws *> char ',') do           -- member loop
+      let _ ← ws *> JSON.string                 -- ws string
+      let _ ← ws *> char ':'                    -- ws ':'
+      let _ ← ws *> JSON.value                  -- ws value ws
+    let _ ← ws <* char '}'                      -- ws '}'
 
-/-- Parse a JSON array -/
+/-- Parse a JSON array
+
+Specification:
+```
+array
+  | '[' ws ']'
+  | '[' elements ']'
+
+elements
+  | element
+  | element ',' elements
+
+element
+  | ws value ws
+```
+-/
 protected partial def array : JSON.Parser Unit :=
   withErrorMessage "expected array" do
-    let _ ← char '['
-    let _ ← sepBy (char ',') do
-      let _ ← ws *> JSON.value
-    let _ ← ws <* char ']'
+    let _ ← char '['                            -- '['
+    let _ ← sepBy (char ',') do                 -- element loop
+      let _ ← ws *> JSON.value                  -- ws value ws
+    let _ ← ws *> char ']'                      -- ws ']'
 
-/-- Parse a JSON value -/
-protected partial def value : JSON.Parser Unit :=
-  withErrorMessage "expected value" do
-    let c ← lookAhead anyToken
-    if c == 'f' then
-      Char.string "false" *> pure ()
-    else if c == 'n' then
-      Char.string "null" *> pure ()
-    else if c == 't' then
-      Char.string "true" *> pure ()
-    else if c == '\t' then
-      JSON.string
-    else if c == '{' then
-      JSON.object
-    else if c == '[' then
-      JSON.array
-    else if c == '-' || c.isDigit then
-      JSON.number
-    else
-      throwUnexpected (some c)
 
 end
 
 /-- JSON validator -/
 def validate (str : String) : Bool :=
-  match Parser.run (JSON.value <* endOfInput) str with
+  match Parser.run (ws *> JSON.value <* ws <* endOfInput) str with
   | .ok _ _ => true
   | .error _ _ => false
 
