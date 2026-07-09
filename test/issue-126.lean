2e4408dd6a314d4b811b15f8c1b2d7041d800c38
François G. Dorais <fgdorais@gmail.com>
2026-07-08 23:38:45 -0400

    fix: setParser for ByteSlice (#127)

---
+++ test/issue-126.lean
@@ -0,0 +1,22 @@
+module
+
+import Parser
+meta import Parser.Error
+meta import Parser.Parser
+open Parser
+
+namespace Test126
+
+abbrev TestParser := SimpleParser ByteSlice UInt8
+
+def test (tk : Char) : TestParser UInt8 := tokenFilter (· = tk.val.toUInt8)
+
+def run (p : TestParser UInt8) (input : String) : String :=
+  let arr := input.toByteArray.toByteSlice
+  match p.run arr with | .ok _ x => s!"ok {Char.ofUInt8 x}" | .error _ e => s!"error {e}"
+
+/-- info: "ok a" -/
+#guard_msgs in #eval run (test 'a' <|> test 'b') "a"
+
+/-- info: "ok b" -/
+#guard_msgs in #eval run (test 'a' <|> test 'b') "b"
